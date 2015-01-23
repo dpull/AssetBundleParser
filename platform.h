261d6fc6cd5db70e5bd42fe4a843c0253115165c
dpull <beingstudio@gmail.com>
2015-01-23 15:09:49 +0800

    Squashed 'AssetBundle/utils/' content from commit f892e40
    git-subtree-dir: AssetBundle/utils
    git-subtree-split: f892e40e147bcce70089a9ae85b6e65fff137f2f

---
+++ platform.h
@@ -0,0 +1,31 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+#ifdef _MSC_VER
+#ifndef getcwd
+#define getcwd      _getcwd
+#endif
+#define snprintf    _snprintf
+#define snwprintf   _snwprintf
+#define tzset       _tzset
+#define open        _open
+#define close       _close
+#define chsize      _chsize
+#define fileno      _fileno
+#define stricmp     _stricmp
+#ifndef strdup
+#define strdup      _strdup
+#endif
+#ifndef wcsdup
+#define wcsdup      _wcsdup
+#endif
+#define getch       _getch
+#define strtoll     _strtoi64
+#define strtoull    _strtoui64
+#define strtok_r    strtok_s
+#define wfopen      _wfopen
+#define getpid      _getpid
+#define fseek64     _fseeki64
+#define ftell64     _ftelli64
+#endif
+
+#endif
